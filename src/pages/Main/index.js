20f711a10ab13374bd0cd9afaa7b57272760425e
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-04-13 00:58:30 -0300

    Adjust tab spaces

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,23 +5,23 @@ import { FaGithubAlt, FaPlus } from "react-icons/fa";
 import { Container, Form, SubmitButton } from "./styles";
 
 class Main extends Component {
-    render() {
-        return(
-            <Container>
-                <h1>
-                    <FaGithubAlt />
-                    Repositórios
-                </h1>
+	render() {
+		return(
+			<Container>
+				<h1>
+					<FaGithubAlt />
+					Repositórios
+				</h1>
 
-                <Form onSubmit={() => this.handleSubmit}>
-                    <input type="text" placeholder="Repo name..." onChange={() => this.handleChange} />
-                    <SubmitButton>
-                        <FaPlus color="#FFF" size={14} />
-                    </SubmitButton>
-                </Form>
-            </Container>
-        );
-    }
+				<Form onSubmit={() => this.handleSubmit}>
+					<input type="text" placeholder="Repo name..." onChange={() => this.handleChange} />
+					<SubmitButton>
+						<FaPlus color="#FFF" size={14} />
+					</SubmitButton>
+				</Form>
+			</Container>
+		);
+	}
 }
 
 export default Main;
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,49 +1,49 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    max-width: 700px;
-    background: #fff;
-    border-radius: 4px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-    padding: 30px;
-    margin: 80px auto;
+	max-width: 700px;
+	background: #fff;
+	border-radius: 4px;
+	box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+	padding: 30px;
+	margin: 80px auto;
 
-    h1 {
-        font-size: 20px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
+	h1 {
+		font-size: 20px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
 
-        svg {
-            margin-right: 10px;
-        }
-    }
+		svg {
+			margin-right: 10px;
+		}
+	}
 `;
 
 export const Form = styled.form`
-    margin-top: 30px;
-    display: flex;
-    flex-direction: row;
+	margin-top: 30px;
+	display: flex;
+	flex-direction: row;
 
-    input {
-        flex: 1;
-        border: 1px solid #eee;
-        padding: 10px 15px;
-        border-radius: 4px;
-        font-size: 16px;
-    }
+	input {
+		flex: 1;
+		border: 1px solid #eee;
+		padding: 10px 15px;
+		border-radius: 4px;
+		font-size: 16px;
+	}
 `;
 
 export const SubmitButton = styled.button.attrs({
-    type: 'submit'
+	type: 'submit'
 })`
-    background: #7159c1;
-    border: 0;
-    padding: 0 15px;
-    margin-left: 10px;
-    border-radius: 4px;
+	background: #7159c1;
+	border: 0;
+	padding: 0 15px;
+	margin-left: 10px;
+	border-radius: 4px;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
