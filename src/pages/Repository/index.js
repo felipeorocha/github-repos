1cb4328c597b174617bb5cd2eed9e07a12373441
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-04-15 00:45:26 -0300

    Repository component refactor

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { Container } from "./styles";
 
-function Repository({}) {
-    return(
-        <h2></h2>
-    );
+function Repository({ match }) {
+  return(
+    <h2>Repository: {decodeURIComponent(match.params.repository)}</h2>
+  );
+}
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string
+    })
+  }).isRequired
 }
 
 export default Repository;
